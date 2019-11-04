2998e28b15834684afc4f7ae30216f24bda80c43
Vatan Soni <vatansoni89@gmail.com>
2019-11-04 17:12:24 +0530

    basic define delegate and use a function example.

---
+++ EDL-DanWahlin/EDL-DanWahlin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EDL_DanWahlin
+{
+    public delegate void WorkPerformedhandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            WorkPerformedhandler del1 = new WorkPerformedhandler(WorkPerformed1);
+            WorkPerformedhandler del2 = new WorkPerformedhandler(WorkPerformed2);
+            //del1 += WorkPerformed2;
+
+            del1(1, WorkType.Golf);
+            del2(2, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        public static void WorkPerformed1(int h, WorkType w)
+        {
+            Console.WriteLine("WorkPerformed 1 called..."+h.ToString());
+        }
+        public static void WorkPerformed2(int h, WorkType w)
+        {
+            Console.WriteLine("WorkPerformed 2 called..." + h.ToString());
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
